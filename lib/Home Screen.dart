e7f0bf96a5f1d78ec492680af7fff5aaedec4641
sfrabby <fazlerabby6732@gmail.com>
2026-07-19 23:43:51 +0600

    Riverpod counter app

--- lib/Home Screen.dart
+++ lib/Home Screen.dart
@@ -5,10 +5,34 @@ class homeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      backgroundColor: Colors.teal,
-      title: Text("Riverpod", style: TextStyle(color: Colors.white),),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text("Riverpod", style: TextStyle(color: Colors.white)),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
 
-    ),);
+        children: [
+          Center(
+            child: Card(
+              elevation: 09,
+              child: Text(
+                "100",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w800),
+              ),
+            ),
+          ),
+
+          Row(children: [
+            ElevatedButton(onPressed: (){}, child: Text("+")),
+            ElevatedButton(onPressed: (){}, child: Text("-")),
+
+
+          ],)
+        ],
+      ),
+    );
   }
 }
