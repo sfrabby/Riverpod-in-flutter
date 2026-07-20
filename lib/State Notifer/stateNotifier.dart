5ed7c6193539b351df1eef25da85e61f1c58906e
sfrabby <fazlerabby6732@gmail.com>
2026-07-20 23:57:57 +0600

    State Notifier

---
+++ lib/State Notifer/stateNotifier.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class stateNotifer extends StatelessWidget {
+  const stateNotifer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.teal,
+      title: Text("State Notifier"),),
+    );
+  }
+}
