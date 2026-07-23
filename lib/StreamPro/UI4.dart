f14fb776c16e2e0797cd73010429104c7eeee302
sfrabby <fazlerabby6732@gmail.com>
2026-07-23 23:56:06 +0600

    Stream Provider UI added

---
+++ lib/StreamPro/UI4.dart
@@ -0,0 +1,29 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'StreamPro.dart';
+
+class ClockScreen extends ConsumerWidget {
+  const ClockScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final clock = ref.watch(clockProvider);
+
+    return Scaffold(
+      body: Center(
+        child: clock.when(
+          data: (time) => Text(
+            time.toString(),
+            style: const TextStyle(fontSize: 25),
+          ),
+          loading: () => const CircularProgressIndicator(),
+          error: (e, s) => Text(e.toString()),
+        ),
+      ),
+    );
+  }
+}
